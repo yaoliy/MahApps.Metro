6a7b97b2244515121c1916e5dd029854dc0f9fd9
Brendan Forster <me@brendanforster.com>
2013-05-23 23:02:23 +1000

    added repro from @rguerraalmeida to demonstrate #456

---
+++ ListBoxBindingIssue/ErrorSampleViewModel.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace MahApps.MetroUI.ListboxItemStylesBug
+{
+    public class Person
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public double Money { get; set; }
+    }
+
+    public class ErrorSampleViewModel: INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged
+
+        protected void RaisePropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+
+
+        private int windowID;
+        public int WindowID
+        {
+            get { return windowID; }
+            set { windowID = value; RaisePropertyChanged("WindowID"); }
+        }
+
+        private string windowTitle;
+        public string WindowTitle
+        {
+            get { return windowTitle; }
+            set { windowTitle = value; RaisePropertyChanged("WindowTitle"); }
+        }
+
+        private double windowRating;
+        public double WindowRating
+        {
+            get { return windowRating; }
+            set { windowRating = value; RaisePropertyChanged("WindowRating"); }
+        }
+
+        private ObservableCollection<Person> _persons;
+        public ObservableCollection<Person> Persons
+        {
+            get { return _persons; }
+            set { _persons = value; RaisePropertyChanged("Persons"); }
+        }
+
+        public ErrorSampleViewModel()
+        {
+            WindowID = 59909870;
+            WindowTitle = "Just a Name for my window";
+            WindowRating = 99.680;
+            Persons = Helpers.GetData();
+        }
+        
+    }
+}
---
+++ ListBoxBindingIssue/Helpers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace MahApps.MetroUI.ListboxItemStylesBug
+{
+    public static class Helpers
+    {
+        public static double GetRandomNumber(double minimum, double maximum)
+        {
+            Random random = new Random();
+            return random.NextDouble() * (maximum - minimum) + minimum;
+        }
+
+        public static ObservableCollection<Person> GetData()
+        {
+            var persons = new ObservableCollection<Person>();
+            for (int i = 0; i < 30; i++)
+            {
+                persons.Add(new Person() 
+                { 
+                    ID = i,
+                    Name = string.Concat("Person ",i),
+                    Money = Helpers.GetRandomNumber(10, 100),
+                });
+            }
+            return persons;
+        }
+
+    }
+}
